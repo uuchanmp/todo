d5c7b426ec70195c9b2676492425179f4083602f
Jiawei <stevecair@gmail.com>
2025-01-25 00:00:46 -0500

    update test cases for app component

--- examples/angular/src/app/app.component.spec.ts
+++ examples/angular/src/app/app.component.spec.ts
@@ -1,16 +1,45 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { FooterComponent } from './footer/footer.component';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        AppComponent,
+        HeaderComponent,
+        TodoListComponent,
+        FooterComponent,
+      ],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  it('should create the app component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should render the header component', () => {
+    const headerElement = fixture.debugElement.query(By.directive(HeaderComponent));
+    expect(headerElement).toBeTruthy();
+  });
+
+  it('should render the todo list component', () => {
+    const todoListElement = fixture.debugElement.query(By.directive(TodoListComponent));
+    expect(todoListElement).toBeTruthy();
+  });
+
+  it('should render the footer component', () => {
+    const footerElement = fixture.debugElement.query(By.directive(FooterComponent));
+    expect(footerElement).toBeTruthy();
   });
 });
